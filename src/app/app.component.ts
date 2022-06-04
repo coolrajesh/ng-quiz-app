07e17a09184e0e88ffa16e71ca214b30959ec870
rajeshroy8686 <rajeshroy8686@gmail.com>
2022-06-04 20:19:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import questionData from '../app/files/questions.json';  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-app';
+  answerArray = [];
+	allQuestions:any[]=questionData;
+  currentIndex=0;
+  currentQuestion:any='';
+  counter:number=0;   
+  ngOnInit(){
+    
+    console.log(this.allQuestions);
+    this.currentQuestion  = this.allQuestions[this.currentIndex].question;
+
+  }
+  nextQuestion(){
+      this.counter++;
+      this.currentIndex = this.counter;
+      this.currentQuestion  = this.allQuestions[this.currentIndex].question;
+  }
+
+  prevQuestion(){
+    this.counter--;    
+    this.currentIndex = this.counter;
+    this.currentQuestion  = this.allQuestions[this.currentIndex].question;
+}
 }
